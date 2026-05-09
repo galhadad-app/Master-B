d5d6cba512e36979e1910245edc6657d866498f0
galhadad-app <galhadad89@gmail.com>
2026-05-09 23:39:20 +0300

    Update index.js

--- functions/index.js
+++ functions/index.js
@@ -1,7 +1,6 @@
 const functions = require("firebase-functions");
 const admin = require("firebase-admin");
 const express = require("express");
-const cors = require("cors");
 const axios = require("axios");
 const { AsyncLocalStorage } = require("async_hooks");
 
@@ -9,8 +8,13 @@ admin.initializeApp();
 
 const db = admin.firestore();
 const app = express();
-app.use(cors({ origin: true }));
-app.options("*", cors({ origin: true }));
+app.use((req, res, next) => {
+  res.set("Access-Control-Allow-Origin", "*");
+  res.set("Access-Control-Allow-Methods", "GET,POST,OPTIONS");
+  res.set("Access-Control-Allow-Headers", "Content-Type,Authorization");
+  if (req.method === "OPTIONS") return res.status(204).send("");
+  return next();
+});
 app.use(express.json({ limit: "2mb" }));
 
 const whatsappContext = new AsyncLocalStorage();
